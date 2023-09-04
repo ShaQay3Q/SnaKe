edb00433ae2616926f48c778e4c7801444f74d1e
Sha <shaghay3gh@gmail.com>
2023-09-04 20:54:19 +0200

    try to shorten the codeS!

--- snake.go
+++ snake.go
@@ -11,6 +11,8 @@ type snake struct {
 	direction direction
 }
 
+// Homework: re-right this function
+
 func move(s snake) (ret_s snake) {
 	if s.direction == "up" {
 		ret_s = snake{
@@ -40,7 +42,8 @@ func move(s snake) (ret_s snake) {
 }
 
 func changeDirection(s snake, d direction) (ret_s snake) {
-	snake_d := snakeDirection(s, d)
+	//snake_d := snakeDirection(s, d)
+	snake_d := banana(s, d)
 	ret_s = snake{
 		body:      []position{{x: s.body[0].x, y: s.body[0].y}},
 		direction: snake_d,
@@ -75,4 +78,27 @@ func snakeDirection(s snake, d direction) (ret_d direction) {
 		}
 	}
 	return
+}
+
+func banana(s snake, d direction) direction {
+	switch s.direction {
+	case "up":
+		if d == "down" {
+			return "up"
+		}
+	case "down":
+		if d == "up" {
+			return "down"
+		}
+	case "right":
+		if d == "left" {
+			return "right"
+		}
+	case "left":
+		if d == "right" {
+			return "left"
+		}
+	}
+
+	return d
 }
