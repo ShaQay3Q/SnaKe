8e27b809f7b26d866e107db24d5afd37b846fc91
Sha <shaghay3gh@gmail.com>
2023-09-08 12:40:48 +0200

    removing unneccessary codes, keeping the shortened ones. -> No BANANAS LEFT!

--- snake.go
+++ snake.go
@@ -11,8 +11,7 @@ type snake struct {
 	direction direction
 }
 
-// Homework: re-right this function
-func bananaMove(s snake) snake {
+func move(s snake) snake {
 	s = snake{
 		body:      []position{{x: s.body[0].x, y: s.body[0].y}},
 		direction: s.direction,
@@ -30,37 +29,9 @@ func bananaMove(s snake) snake {
 	return s
 }
 
-func move(s snake) (ret_s snake) {
-	if s.direction == "up" {
-		ret_s = snake{
-			body:      []position{{x: s.body[0].x, y: s.body[0].y - 1}},
-			direction: direction("up"),
-		}
-		return
-	} else if s.direction == "down" {
-		ret_s = snake{
-			body:      []position{{x: s.body[0].x, y: s.body[0].y + 1}},
-			direction: direction("down"),
-		}
-		return
-	} else if s.direction == "right" {
-		ret_s = snake{
-			body:      []position{{x: s.body[0].x + 1, y: s.body[0].y}},
-			direction: direction("right"),
-		}
-		return
-	} else {
-		ret_s = snake{
-			body:      []position{{x: s.body[0].x - 1, y: s.body[0].y}},
-			direction: direction("left"),
-		}
-		return
-	}
-}
-
 func changeDirection(s snake, d direction) (ret_s snake) {
 	//snake_d := snakeDirection(s, d)
-	snake_d := banana(s, d)
+	snake_d := snakeDirection(s, d)
 	ret_s = snake{
 		body:      []position{{x: s.body[0].x, y: s.body[0].y}},
 		direction: snake_d,
@@ -68,36 +39,7 @@ func changeDirection(s snake, d direction) (ret_s snake) {
 	return
 }
 
-func snakeDirection(s snake, d direction) (ret_d direction) {
-	if s.direction == "up" {
-		if d == "down" {
-			ret_d = "up"
-		} else {
-			ret_d = d
-		}
-	} else if s.direction == "down" {
-		if d == "up" {
-			ret_d = "down"
-		} else {
-			ret_d = d
-		}
-	} else if s.direction == "right" {
-		if d == "left" {
-			ret_d = "right"
-		} else {
-			ret_d = d
-		}
-	} else {
-		if d == "righ" {
-			ret_d = "left"
-		} else {
-			ret_d = d
-		}
-	}
-	return
-}
-
-func banana(s snake, d direction) direction {
+func snakeDirection(s snake, d direction) direction {
 	switch s.direction {
 	case "up":
 		if d == "down" {
@@ -116,6 +58,5 @@ func banana(s snake, d direction) direction {
 			return "left"
 		}
 	}
-
 	return d
 }
